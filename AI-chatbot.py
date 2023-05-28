f328fe4df761057ac44ea23da02aeb5db6eebcb0
NamrataDatta04 <87001240+NamrataDatta04@users.noreply.github.com>
2023-05-29 04:33:16 +0600

    Update AI-chatbot.py

--- AI-chatbot.py
+++ AI-chatbot.py
@@ -1,11 +1,11 @@
 import openai
 import time
 import string
-from functools import lru_cache
+import requests
+from newsapi import NewsApiClient
 
 # Set OpenAI API key
-openai.api_key = "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
-"
+openai.api_key = "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"
 
 # Initialize chat messages list
 messages = []
@@ -16,11 +16,13 @@ messages.append({"role": "system", "content": system_msg})
 
 print("Your new assistant is ready!")
 
+# Get NewsAPI API key
+newsapi = NewsApiClient(api_key="e74d9b9325294830b0c64dd4b2dad4fa")
+
 # List to store response times for monitoring
 response_times = []
 
 # Function to process user input data
-@lru_cache(maxsize=128)  # Caching function results
 def process_data(input_data):
     # Convert input to lowercase
     processed_data = input_data.lower()
@@ -32,6 +34,19 @@ def process_data(input_data):
     tokens = processed_data.split()
    
     return processed_data
+
+# Function to fetch the latest news from NewsAPI
+def fetch_latest_information():
+    news_data = newsapi.get_everything(q="bitcoin", sources="bbc-news,the-verge", domains="bbc.co.uk,techcrunch.com", language="en", sort_by="relevancy", page=2)
+
+    articles = news_data.get("articles")
+
+    if articles:
+        latest_article = articles[0]
+        latest_information = latest_article.get("title")
+        return latest_information
+
+    return None
 
 # Main chat loop
 while True:
@@ -48,10 +63,21 @@ while True:
 
         start_time = time.time()  # Start measuring response time
 
+        # Fetch the latest information
+        latest_information = fetch_latest_information()
+
+        if latest_information:
+            messages.append({"role": "system", "content": latest_information})
+
         # Get response from OpenAI Chat API
         response = openai.ChatCompletion.create(
             model="gpt-3.5-turbo",
-            messages=messages
+            messages=messages,
+            temperature=0.6,  # Controls the randomness of the output
+            max_tokens=100,  # Controls the length of the output
+            n=1,  # Generate a single response
+            stop=None,  # Stop generating after reaching a specific token (optional)
+            
         )
 
         end_time = time.time()  # Stop measuring response time
@@ -59,23 +85,18 @@ while True:
         response_times.append(response_time)  # Store response time for monitoring
 
         # Get reply from the response
-        reply = response["choices"][0]["message"]["content"]
+        reply = response.choices[0].message.content
         messages.append({"role": "assistant", "content": reply})
         print("\n" + reply + "\n")
 
         # Process user input
         processed_input = process_data(user_input)
 
-        # Cache frequently accessed results
-        processed_result = process_data.cache_info()
-        if processed_result.hits > 10:
-            pass
-        
         # Feedback and learning
         if user_input != reply:
             # Prompt the user for feedback
             feedback = input("Was the assistant's response helpful? [yes/no]: ")
-            
+
             # Provide positive or negative feedback
             if feedback.lower() == "yes":
                 # Positive feedback, reward the model
@@ -89,14 +110,16 @@ while True:
             else:
                 # Negative feedback, train the model on correct response
                 correct_response = input("Please provide the correct response: ")
-                openai.ChatCompletion.create(
-                    model="gpt-3.5-turbo",
-                    messages=[
-                        {"role": "user", "content": user_input},
-                        {"role": "assistant", "content": correct_response}
-                    ]
-                )
-    
+                messages.append({"role": "user", "content": user_input})
+                messages.append({"role": "assistant", "content": correct_response})
+                # Save conversation history to a file
+                with open("conversation_history.txt", "a") as f:
+                    for message in messages:
+                        f.write(f"{message['role']}: {message['content']}\n")
+                # Reset chat messages
+                messages = []
+                messages.append({"role": "system", "content": system_msg})
+
     # Ask another question or exit
     print("Press Enter to ask a new question or type 'quit()' to exit.")
     user_input = input()
